e81fa98ea5903f1c0fc56a07178f433f8f6d8ecd
ShawnL <shawn.lws1225@gmail.com>
2021-01-27 02:59:47 +0800

    remove .exe file

---
+++ ForFun/1a2b.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string ans;
+    string input;
+    cout << "input ans: "<<endl;
+    cin >> ans;
+    while(cin >> input){
+        int ctA=0,ctB=0;
+
+        for(int i=0;i<4;i++){
+            if(ans[i] == input[i]) {
+                ctA++;
+                input[i] = '*';
+            }
+        }
+
+        for(int i=0;i<4;i++){
+            if(input[i] == '*') continue;
+            
+            for(int j=0;j<4;j++){
+                if(input[j] == '*') continue;
+                if(ans[i] == input[j]){
+                    ctB++;
+                    input[j] = '^';
+                }
+            }
+        }
+        
+        if(ctA == 4) {
+            cout << "congrats !"<<endl;
+            return 0;
+        }
+        else
+            cout << ctA << 'A' << ctB << "B\n\n";
+    }
+}
