a00838b1d64d3fa944d26c888cc3e122548d7e0d
ShawnL <shawn.lws1225@gmail.com>
2021-01-27 03:05:25 +0800

    gitignore

--- ForFun/1a2b.cpp
+++ ForFun/1a2b.cpp
@@ -9,18 +9,18 @@ int main()
     cin >> ans;
     while(cin >> input){
         int ctA=0,ctB=0;
-
-        for(int i=0;i<4;i++){
+        int len = ans.length();
+        for(int i=0; i<len; i++){
             if(ans[i] == input[i]) {
                 ctA++;
                 input[i] = '*';
             }
         }
 
-        for(int i=0;i<4;i++){
+        for(int i=0; i<len; i++){18
             if(input[i] == '*') continue;
             
-            for(int j=0;j<4;j++){
+            for(int j=0; j<len; j++){
                 if(input[j] == '*') continue;
                 if(ans[i] == input[j]){
                     ctB++;
