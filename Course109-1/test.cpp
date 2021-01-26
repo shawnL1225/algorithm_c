d843758765d7ed81c45f37c06ef0d5ebbc27c10c
ShawnL <shawn.lws1225@gmail.com>
2021-01-27 02:38:02 +0800

    use ./a in vscode

--- Course109-1/test.cpp
+++ Course109-1/test.cpp
@@ -3,35 +3,5 @@ using namespace std;
 
 int main()
 {
-    string ans;
-    string input;
-    cout << "input ans: "<<endl;
-    cin >> ans;
-    while(cin >> input){
-        int ctA=0,ctB=0;
-
-        for(int i=0;i<4;i++){
-            if(ans[i] == input[i]) {
-                ctA++;
-                input[i] = '*';
-            }
-        }
-
-        for(int i=0;i<4;i++){
-            for(int j=0;j<4;j++){
-                if(i==j) continue;
-                if(ans[i] == input[j]){
-                    ctB++;
-                    input[j] = '*';
-                }
-            }
-        }
-        
-        if(ctA == 4) {
-            cout << "congrats !"<<endl;
-            return 0;
-        }
-        else
-            cout << ctA << 'A' << ctB << 'B'<<endl;
-    }
+    cout << "hello Woreeeld"<<endl;
 }
