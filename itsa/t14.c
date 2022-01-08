5f2d75fa455358e92e22c2868833cfdbb8a29251
shawnL <shawnL@csie.io>
2022-01-08 20:27:53 +0800

    itsa

--- itsa/t14.c
+++ itsa/t14.c
@@ -1,9 +1,9 @@
-#include <bits/stdc++.h>
+#include <stdio.h>
 
 int main(){
     char s[60];
     while(scanf("%c",s)){
-        bool suc = 1;
+        int suc = 1;
         int len = strlen(s);
         for(int i=0;i<len;i++){
             if(s[i]!=s[len-1-i]){
